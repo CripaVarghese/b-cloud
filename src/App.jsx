44ab8b56546ef51afaf9a3772fcc0da24e1794a9
Cripa Varghese <cripavarghese@Cripas-MacBook-Air.local>
2023-12-04 12:06:28 +0530

    feat: Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
-import "./App.css";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import { Divider, Box, Stack } from "@chakra-ui/react";
-import Hero from "./components/Hero";
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Stack
-      backgroundColor={{
-        base: "blue",
-        sm: "pink",
-        md: "green",
-        lg: "yellow",
-        "2xl": "red",
-      }}
-    >
-      <Stack flexDirection={"column"} alignItems={"center"} height={"100%"}>
-        <Box
-          w={"100%"}
-          mb={3}
-          boxShadow={"rgba(0, 0, 0, 0.05) 0px 1px 2px 0px"}
-        >
-          <Header />
-          <Divider />
-          <Navbar />
-        </Box>
-        <Box w={{ md: "90%", lg: "92.5%", "2xl": "80%" }}>
-          <Hero />
-        </Box>
-      </Stack>
-    </Stack>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
