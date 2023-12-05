d32f6c05d9178ca3e284bbf67539c320a919004b
Cripa Varghese <cripavarghese@Cripas-MacBook-Air.local>
2023-12-06 00:01:37 +0530

    feat: component

---
+++ src/components/Products.jsx
@@ -0,0 +1,27 @@
+import Sidebar from "./Sidebar";
+import { Stack, Text } from "@chakra-ui/react";
+import Details from "./Details";
+import { productDetailsStyles as style } from "../styles";
+
+const Products = () => {
+  return (
+    <Stack alignItems={{ base: "center", md: "flex-start" }}>
+      <Text color={"#0063FF"} {...style.lgFontStyles}>
+        Product details
+      </Text>
+
+      <Stack
+        w={{ "2xl": "100%" }}
+        h={{ lg: "113vh", "2xl": "100%" }}
+        flexDirection={{ base: "column", md: "row", lg: "row" }}
+        justifyContent={"space-between"}
+        alignItems={{ base: "center", md: "flex-start" }}
+      >
+        <Sidebar />
+        <Details />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Products;
