c10bdafed3020846ae20fde349b65c89866edd50
Cripa Varghese <cripavarghese@Cripas-MacBook-Air.local>
2023-12-06 14:49:10 +0530

    feat: modified App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import Home from "./components/Home";
-import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { Box, Divider, Stack } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import UnauthenticatedRoutes from "./routes/UnauthenticatedRoutes";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Stack flexDirection={"column"} alignItems={"center"} height={"100%"}>
+        <Box
+          w={"100%"}
+          mb={3}
+          boxShadow={"rgba(0, 0, 0, 0.05) 0px 1px 2px 0px"}
+          position={"fixed"}
+          zIndex={"1"}
+          bg="white"
+        >
+          <Header />
+          <Divider />
+          <Navbar />
+        </Box>
+
+        <Box w={{ md: "90%", lg: "92.5%", "2xl": "80%" }} pt="130px">
+          <UnauthenticatedRoutes />
+        </Box>
+      </Stack>
+    </>
   );
 }
 
