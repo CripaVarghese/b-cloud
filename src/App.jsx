504fd36f14add2dbfa91217ba08f27702a399573
Cripa Varghese <cripavarghese@Cripas-MacBook-Air.local>
2023-12-08 03:34:16 +0530

    fix: route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import { URL_PATHS } from "./constants/routes";
+import URL_PATHS from "./constants/routes";
+
 import Products from "./components/Products";
 import AppLayout from "./components/AppLayout";
 import Details from "./components/Details";
