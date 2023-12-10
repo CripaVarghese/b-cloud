57b340464335dd500dfa0e3f8c26454512bee1eb
cripav <cripav96@gmail.com>
2023-12-11 03:18:17 +0530

    feat: active button

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,14 +2,24 @@ import { Button, HStack, Stack, Text } from "@chakra-ui/react";
 import buttonList from "../dataLists/buttonList";
 import { useNavigate } from "react-router-dom";
 import { style } from "../styles";
+import { useState } from "react";
 
 const List = () => {
   const navigate = useNavigate();
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleClick = (id) => {
+    setActiveButton(id);
+  };
   return (
     <Stack justifyContent={"space-between"} h="100%" gap="3">
-      {buttonList.map((list, i) => (
+      {buttonList.map((list, index) => (
         <HStack
-          key={i}
+          key={index}
+          style={{
+            backgroundColor:
+              activeButton === index ? style.themeColor.bg : "transparent",
+          }}
           w="100%"
           justifyContent={"flex-start"}
           _hover={{ bg: style.themeColor.bg, color: "white" }}
@@ -17,14 +27,17 @@ const List = () => {
           border={".5px solid #7FB2FF"}
           borderRadius={5}
           gap="0"
-          _active={{ bg: style.themeColor.bg, color: "white" }}
           onClick={() => {
+            handleClick(index);
             navigate(`/products/${list.href}`);
           }}
           className="sidebar-button"
         >
           <Button variant={"ghost"} _hover={{ variant: "ghost" }}>
-            <list.svg className="icon" color={style.themeColor.color} />
+            <list.svg
+              className="icon"
+              color={activeButton === index ? "white" : style.themeColor.bg}
+            />
           </Button>
           <Text
             w="full"
@@ -33,6 +46,9 @@ const List = () => {
             variant="ghost"
             fontSize={"11"}
             fontWeight={"semibold"}
+            style={{
+              color: activeButton === index ? "white" : style.themeColor.color,
+            }}
             color={style.themeColor.color}
             className="text-button"
           >
